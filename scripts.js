9e386ae096dd9cca0fd034b5f12c701b1d10c06d
unknown <kjmalley7@gmail.com>
2023-11-03 08:08:19 -1000

    ops: init'ing remote repo

---
+++ scripts.js
@@ -0,0 +1,55 @@
+let currentDate = new Date()
+let day = String(currentDate.getDate()).padStart(2, '0')
+let month = String(currentDate.getMonth() + 1).padStart(2, '0')
+let year = currentDate.getFullYear()
+
+currentDate = year + '-' + month + '-' + day
+
+document.getElementById('date').value = currentDate
+
+document
+	.getElementById('inventoryForm')
+	.addEventListener('submit', function (event) {
+		event.preventDefault()
+
+		let date = document.getElementById('date').value
+		let containerNumber = document.getElementById('container-number').value
+		let bookingNumber = document.getElementById('booking-number').value
+
+		document.getElementById('tableTitle').innerHTML =
+			'Container Number: ' + containerNumber
+		document.getElementById('tableSubtitle').innerHTML =
+			'Booking Number: ' + bookingNumber
+		document.getElementById('tableDate').innerHTML = 'Date: ' + date
+	})
+
+function addEntryToTable() {
+	// Get the table's tbody element
+	const tbody = document
+		.getElementById('entries-table')
+		.getElementsByTagName('tbody')[0]
+
+	// Create a new row
+	const newRow = tbody.insertRow()
+
+	// Get form data
+	const jobNumber = document.getElementById('job-number').value
+	const customerName = document.getElementById('customer-name').value
+	const quantity = document.getElementById('quantity').value
+	const manufacturer = document.getElementById('manufacturer').value
+	const description = document.getElementById('description').value
+	const orderNumber = document.getElementById('order-number').value
+	const notes = document.getElementById('notes').value
+
+	// Populate the row with form data
+	newRow.insertCell(0).innerText = jobNumber
+	newRow.insertCell(1).innerText = customerName
+	newRow.insertCell(2).innerText = quantity
+	newRow.insertCell(3).innerText = manufacturer
+	newRow.insertCell(5).innerText = description
+	newRow.insertCell(6).innerText = orderNumber
+	newRow.insertCell(7).innerText = notes
+
+	// Optionally, clear the form after adding the entry
+	document.getElementById('inventoryForm').reset()
+}
